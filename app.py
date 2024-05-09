331596882c161f630d0911cbfdd78450eb402ec5
Atul Vinod <atulvinod2001@gmail.com>
2024-05-09 10:32:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, request, jsonify, session, redirect, url_for, render_template, abort, send_file
+from flask import Flask, request, jsonify, session, redirect, url_for, render_template, abort, send_file, Response
 from web3 import Web3, HTTPProvider
 from os import environ
 from eth_account.messages import encode_defunct
@@ -63,9 +63,10 @@ def register():
 @app.route('/dashboard')
 def dashboard():
     if 'user' not in session:
-        logging.warning("Unauthorized access attempt to dashboard.")
         return redirect(url_for('login'))
-    return render_template('dashboard.html', user=session['user'])
+    user_address = session['user']  # Retrieve the user address from the session
+    return render_template('dashboard.html', user_address=user_address)
+
 
 @app.route('/logout')
 def logout():
@@ -88,7 +89,6 @@ def show_download_page():
 
 @app.route('/download/file', methods=['GET'])
 def download_file():
-    # This route handles the actual file download.
     hash_value = request.args.get('hash')
     if not hash_value:
         return jsonify({'error': 'No hash provided'}), 400
@@ -97,13 +97,11 @@ def download_file():
         file_data = client.cat(hash_value)
         return send_file(
             io.BytesIO(file_data),
-            attachment_filename=f"{hash_value}.ipfs",  # Assuming .ipfs; adjust as needed
-            as_attachment=True
+            mimetype='application/octet-stream',  # Ensures the browser treats it as a binary file
+            as_attachment=True,
+            download_name=f"{hash_value}.ipfs"  # Suggests a default filename for the download
         )
     except Exception as e:
-        logging.error(f"Failed to download file: {e}")
         return jsonify({'error': str(e)}), 404
-
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
